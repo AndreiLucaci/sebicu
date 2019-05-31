5a71110cfc051b8ed3a223d2bac50afbe0104ad6
Lucaci Andrei <lucaciandrei@outlook.com>
2019-05-31 14:40:00 +0300

    added lambda expressions

---
+++ LambdaExpressions/LambdaExpressions/Processors/ILambdaProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions.Processors
+{
+    public interface ILambdaProcessor
+    {
+        int CountMonitoredDays();
+
+        Dictionary<string, int> CountMonitoredDaysPerActivity();
+
+        IEnumerable<Tuple<DateTime, string, int>> CountPerDayEachActivity();
+
+        Dictionary<string, TimeSpan> CountActivityDuration();
+
+        IEnumerable<string> FilterActivitiesWithLessThen5Minutes();
+
+        Task ProcessAsync(string filename);
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using LambdaExpressions.Processors;
+
+namespace LambdaExpressions
+{
+    public class Program
+    {
+        private const string FileName = "Activities.txt";
+
+        static void Main(string[] args)
+        {
+            ILambdaProcessor lambdaProcessor = new LambdaProcessor();
+
+            lambdaProcessor.ProcessAsync(FileName).GetAwaiter().GetResult();
+
+            var activityDuration = lambdaProcessor.CountActivityDuration();
+
+            var monitoredDays = lambdaProcessor.CountMonitoredDays();
+
+            var monitoredDaysPerActivity = lambdaProcessor.CountMonitoredDaysPerActivity();
+
+            var eachActivityPerDay = lambdaProcessor.CountPerDayEachActivity();
+
+            var filteredActivities = lambdaProcessor.FilterActivitiesWithLessThen5Minutes();
+        }
+    }
+}
